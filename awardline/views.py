33ae85805b836d724e86c2cd63f480085937256c
Antony <amwaniki180@gmail.com>
2019-08-05 07:52:24 +0300

    updated view function with an api function

--- awardline/views.py
+++ awardline/views.py
@@ -48,3 +48,54 @@ def profile(request):
     projects = Project.objects.all().filter(poster_id=user.id)
     return render(request, 'profile.html',{"user":user, "current_user":request.user,"projects":projects})
 
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    profile = User.objects.get(username=username)
+    profile_details = Profile.get_by_id(profile.id)
+    
+    projects= Project.get_profile_images(profile.id)
+    
+    is_followed = False
+    if profile.follows.filter(id=request.user.id).exists():
+        is_followed = True
+
+    return render(request, 'user_profile.html', { 'profile':profile, 'profile_details':profile_details, 'projects':projects})
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST,request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.poster= current_user
+            project.save()
+        return redirect('home')
+    else:
+        form = ProjectForm()
+
+    return render(request,'new_project.html',{'form':form})
+
+
+def search(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        projects = Project.search_project(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'projects':projects})
+    else:
+        message = 'Enter term to search'
+        return render(request, 'search.html', {'message':message})
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_project = Project.objects.all()
+        serializers = ProjectSerializer(all_project, many=True)
+        return Response(serializers.data)
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)
