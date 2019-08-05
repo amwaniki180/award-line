78a0ff7b87c8b2d77d788f645dfda6532da2fc78
Antony <amwaniki180@gmail.com>
2019-08-05 07:47:29 +0300

    view functionality

--- awardline/views.py
+++ awardline/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm,ReviewForm,CommentForm
+from .models import Project,Profile,Comments,Review
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
 # Create your views here.
-def welcome(request):
-    return render(request, 'index.html')
+def index(request):
+
+    current_user = request.user
+    projects = Project.objects.order_by('-date')
+    profile = Profile.objects.order_by('-last_update')
+    return render(request,'index.html',{"projects":projects,"profile":profile})
