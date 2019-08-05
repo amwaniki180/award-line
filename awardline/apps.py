dceb9e42d52ec14fff16edcb325c68a7e5e67fb8
Antony <amwaniki180@gmail.com>
2019-08-05 15:10:19 +0300

    added awardline

--- awardline/apps.py
+++ awardline/apps.py
@@ -1,8 +1,5 @@
-from django.contrib import admin
-from .models import Project,Profile,Comments,Review
-# Register your models here.
+from django.apps import AppConfig
 
-admin.site.register(Project)
-admin.site.register(Profile)
-admin.site.register(Comments)
-admin.site.register(Review)
+
+class ProjectsConfig(AppConfig):
+    name = 'awardline'
