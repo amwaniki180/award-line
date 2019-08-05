a2e2b8aa80fef15fd5bcbb059993c700f540f8b9
Antony <amwaniki180@gmail.com>
2019-08-05 07:57:42 +0300

    urls.py set up api

--- awardline/urls.py
+++ awardline/urls.py
@@ -2,5 +2,17 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.index,name = 'welcome'),
+    url(r'^project/(\d+)', views.project, name = "project"),
+    url(r'^add/project',views.new_project, name= 'add_project'),
+    url(r'^profile',views.profile, name= 'profile'),
+    url(r'^user/(?P<username>\w+)', views.user_profile, name = "user_profile" ),
+    url(r'^profile',views.profile, name= 'profile'),
+    url(r'^search/', views.search, name='search'),
+    url(r'^api/profile/$', views.ProfileList.as_view()),
+    url(r'^api/project/$', views.ProjectList.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
