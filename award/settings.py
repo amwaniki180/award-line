eb32e7536637aacab4928318d4ec8bdfd6af59ed
Antony <amwaniki180@gmail.com>
2019-08-05 10:43:27 +0300

    user profile

--- award/settings.py
+++ award/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'awardline.apps.AwardlineConfig',
     'bootstrap4',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
